82ada3a2cc606a669ad6368a30805b4120b3065a
Rif <chatdanai@skooldio.com>
2024-04-26 22:02:17 +0700

    :rocket: project scaffolding

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      ibmPlexSansThaiLooped: ["IBM Plex Sans Thai Looped", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
